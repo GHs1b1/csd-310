bcc8818ca771f9e51f0a4aeb8b58cb53aa224d1f
GHs1b1 <71919639+GHs1b1@users.noreply.github.com>
2021-12-14 18:29:35 -0800

    Update mongodb_test.py

--- mongodb_test.py
+++ mongodb_test.py
@@ -1,1 +1,12 @@
 
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.x6urf.mongodb.net/pytech?";
+
+client = MongoClient(url)
+
+client = MongoClient "mongodb+srv://admin:admin@cluster0.x6urf.mongodb.net/pytech?";
+
+db = client.pytech
+
+print(db.list_collection_names)
